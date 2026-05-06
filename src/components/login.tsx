066d6c6575565842183fa70e62f97986af3d4634
Mohieddine Drissi <m.drissi@protonmail.com>
2026-05-06 16:06:08 +0200

    feat: Login to mlsolid with Google
    This PR implements authentication with mlsolid service through http-only
    cookies and google authentication.

---
+++ src/components/login.tsx
@@ -0,0 +1,90 @@
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Field, FieldDescription, FieldGroup } from "@/components/ui/field";
+import logo from "@/assets/gopher-out.svg";
+import { useEffect } from "react";
+import { isAuthorized } from "@/api/mlsolid";
+import { useNavigate } from "react-router";
+
+export default function Login({
+  className,
+  ...props
+}: React.ComponentProps<"div">) {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    async function redirect() {
+      if (await isAuthorized()) {
+        navigate("/");
+      }
+    }
+
+    redirect();
+  }, [navigate]);
+
+  return (
+    <div>
+      <div className="flex min-h-svh flex-col items-center justify-center gap-6 bg-muted p-6 md:p-10">
+        <div className="flex w-full max-w-sm flex-col gap-6">
+          <a
+            href="#"
+            className="flex items-center gap-2 self-center font-medium"
+          >
+            <div className="flex size-6 items-center justify-center   text-primary-foreground">
+              <img src={logo} className="size-18"></img>
+            </div>
+            MLSolid
+          </a>
+          <div className={cn("flex flex-col gap-6", className)} {...props}>
+            <Card>
+              <CardHeader className="text-center">
+                <CardTitle className="text-xl">Welcome back</CardTitle>
+                <CardDescription>
+                  Login with your Google account
+                </CardDescription>
+              </CardHeader>
+              <CardContent>
+                <form>
+                  <FieldGroup>
+                    <Field>
+                      <Button
+                        onClick={() => {
+                          window.location.href = `${import.meta.env.VITE_MLSOLID}/login/google`;
+                        }}
+                        variant="outline"
+                        type="button"
+                      >
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          viewBox="0 0 24 24"
+                        >
+                          <path
+                            d="M12.48 10.92v3.28h7.84c-.24 1.84-.853 3.187-1.787 4.133-1.147 1.147-2.933 2.4-6.053 2.4-4.827 0-8.6-3.893-8.6-8.72s3.773-8.72 8.6-8.72c2.6 0 4.507 1.027 5.907 2.347l2.307-2.307C18.747 1.44 16.133 0 12.48 0 5.867 0 .307 5.387.307 12s5.56 12 12.173 12c3.573 0 6.267-1.173 8.373-3.36 2.16-2.16 2.84-5.213 2.84-7.667 0-.76-.053-1.467-.173-2.053H12.48z"
+                            fill="currentColor"
+                          />
+                        </svg>
+                        Login with Google
+                      </Button>
+                    </Field>
+                  </FieldGroup>
+                </form>
+              </CardContent>
+            </Card>
+            <FieldDescription className="px-6 text-center">
+              By clicking continue, you agree to our{" "}
+              <a href="#">Terms of Service</a> and{" "}
+              <a href="#">Privacy Policy</a>.
+            </FieldDescription>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
